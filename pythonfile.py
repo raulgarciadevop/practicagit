56468b78a0a26f1202987423262852917689e763
Brandon Morales <brafe.102596@gmail.com>
2021-02-19 18:13:42 -0600

    elimine

--- pythonfile.py
+++ pythonfile.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-
-
-=======
-val = 5
->>>>>>> 99d121cc4c514d639699817a18a6f2c802f7e590
-
-
-
-import os
-def value(values):
-    val = values
-    print(val)
-value(0)
+val = 1
+b = 10
+12456
